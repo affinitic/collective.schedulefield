c7fbfd2048e3988efd6036a023b3232412abe9e9
FBruynbroeck <francois@affinitic.be>
2018-02-26 15:06:09 +0100

    Add multischedule behavior

--- collective/schedulefield/behavior.py
+++ collective/schedulefield/behavior.py
@@ -19,6 +19,13 @@ from zope.interface import provider
 from collective.schedulefield import _
 from collective.schedulefield.schedule import Schedule
 
+from z3c.form.object import registerFactoryAdapter
+from zope.schema import Date
+from zope.schema import List
+from zope.schema import Object
+from zope.schema import Text
+from zope.schema.fieldproperty import FieldProperty
+
 
 @provider(IFormFieldProvider)
 class IScheduledContent(Interface):
@@ -26,7 +33,7 @@ class IScheduledContent(Interface):
     fieldset(
         'schedule',
         label=_('Schedule'),
-        fields=['schedule', 'test'],
+        fields=['schedule'],
     )
 
     schedule = Schedule(
@@ -45,3 +52,66 @@ class ScheduledContent(object):
 
 class WidgetView(WidgetsView):
     schema = IScheduledContent
+
+
+class IScheduledWithTitle(Interface):
+    """ Helper schema for min and max fields """
+
+    title = Text(
+        title=_(u'Title'),
+        required=False
+    )
+
+    start_date = Date(
+        title=_(u'Start date'),
+        required=False
+    )
+
+    end_date = Date(
+        title=_(u'End date'),
+        required=False
+    )
+
+    schedule = Schedule(
+        title=_(u'Schedule'),
+        required=False,
+    )
+
+
+@implementer(IScheduledWithTitle)
+class ScheduledWithTitle(object):
+    """ScheduledWithTitle"""
+    title = FieldProperty(IScheduledWithTitle['title'])
+    start_date = FieldProperty(IScheduledWithTitle['start_date'])
+    end_date = FieldProperty(IScheduledWithTitle['end_date'])
+    schedule = FieldProperty(IScheduledWithTitle['schedule'])
+
+
+registerFactoryAdapter(IScheduledWithTitle, ScheduledWithTitle)
+
+
+@provider(IFormFieldProvider)
+class IMultiScheduledContent(Interface):
+
+    fieldset(
+        'multischedule',
+        label=_('Multi Schedule'),
+        fields=['schedule', 'multi_schedule'],
+    )
+
+    schedule = Schedule(
+        title=_(u'Schedule'),
+        required=False,
+    )
+
+    multi_schedule = List(
+        title=_(u'Multi Schedule'),
+        value_type=Object(__name__='MultiSchedule', schema=IScheduledWithTitle, required=False),
+        required=False,
+    )
+
+
+@implementer(IMultiScheduledContent)
+@adapter(IDexterityContent)
+class MultiScheduledContent(ScheduledContent):
+    pass
