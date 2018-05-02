a9d9bf7f202c382794d9b33c4fe35a2049c4da4e
FBruynbroeck <francois@affinitic.be>
2018-05-02 12:12:08 +0200

    Update get_schedule_by_date (return closure, special or basic schedule)

--- collective/schedulefield/utils.py
+++ collective/schedulefield/utils.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 from collective.schedulefield.behavior import IMultiScheduledContent
 from collective.schedulefield.behavior import IExceptionalClosureContent
-from datetime import date as dt
 
 
 def get_schedule_by_date(context, date):
@@ -10,6 +9,15 @@ def get_schedule_by_date(context, date):
     :param context: IMultiScheduledContent
     :param date: Date
     """
+    if IExceptionalClosureContent.providedBy(context):
+        dates = getattr(context, 'exceptional_closure', None) or []
+        for d in dates:
+            if date == d.date:
+                return {'title': d.title,
+                        'morningstart': None,
+                        'morningend': None,
+                        'afternoonstart': None,
+                        'afternoonend': None}
     if IMultiScheduledContent.providedBy(context):
         multi_schedule = getattr(context, 'multi_schedule', None) or []
         for i in multi_schedule:
@@ -17,18 +25,13 @@ def get_schedule_by_date(context, date):
             dates = i.dates or []
             for d in dates:
                 if d.start_date <= date <= d.end_date:
+                    schedule = dict(schedule.get(date.strftime("%A").lower()))
+                    schedule.pop('comment')
+                    schedule.update({'title': i.title})
                     return schedule
-        return getattr(context, 'schedule', None)
-
-
-def get_exceptionalclosure_by_date(context, date):
-    """get_exceptionalclosure_by_date
-
-    :param context:
-    :param date:
-    """
-    if IExceptionalClosureContent.providedBy(context):
-        dates = getattr(context, 'exceptional_closure', None) or []
-        for d in dates:
-            if dt.today() == d.date:
-                return d
+        schedule = getattr(context, 'schedule', None)
+        if schedule:
+            schedule = dict(schedule.get(date.strftime("%A").lower()))
+            schedule.pop('comment')
+            schedule.update({'title': None})
+        return schedule
